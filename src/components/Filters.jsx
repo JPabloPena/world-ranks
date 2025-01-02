d65e55bc3ccc3d72e5ebba7e209aed6367ddcb9d
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-01-02 17:23:40 -0500

    feat(Filters): add Filters component

---
+++ src/components/Filters.jsx
@@ -0,0 +1,39 @@
+import './Filters.css'
+
+export function Filters () {
+  const REGIONS = ['Americas', 'Antartic', 'Africa', 'Asia', 'Europe', 'Oceania']
+
+  return (
+    <div className='filters'>
+      <section className='filters__sort-by'>
+        <h3>Sort by</h3>
+        <select>
+          <option value='Name'>Name</option>
+          <option value='Population' selected>Population</option>
+          <option value='Area'>Area</option>
+        </select>
+      </section>
+      <section className='filters__region'>
+        <h3>Region</h3>
+        <ul className='filters__list'>
+          {REGIONS.map(region => (
+            <li key={region}>
+              <button>{region}</button>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className='filters__status'>
+        <h3>Status</h3>
+        <div className='filters__status-option'>
+          <input type='checkbox' value='Member of the United Nations' id='memberUN' />
+          <label className='filters__label'>Member of the United Nations</label>
+        </div>
+        <div className='filters__status-option'>
+          <input type='checkbox' value='Member of the United Nations' id='independent' />
+          <label className='filters__label'>Independent</label>
+        </div>
+      </section>
+    </div>
+  )
+}
