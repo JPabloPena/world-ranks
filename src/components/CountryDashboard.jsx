d52648dbbf75209db4456a3269d594c50903e2ae
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-01-04 18:06:58 -0500

    feat(CountryDashboard): use CountriesProvider

--- src/components/CountryDashboard.jsx
+++ src/components/CountryDashboard.jsx
@@ -2,15 +2,18 @@ import './CountryDashboard.css'
 import { Header } from './Header'
 import { CountriesTable } from './CountriesTable'
 import { Filters } from './Filters'
+import { CountriesProvider } from '../contexts/countriesContext'
 
 export function CountryDashboard () {
   return (
-    <main className='country-dashboard'>
-      <Header />
-      <div className='country-dashboard__content'>
-        <Filters />
-        <CountriesTable />
-      </div>
-    </main>
+    <CountriesProvider>
+      <main className='country-dashboard'>
+        <Header />
+        <div className='country-dashboard__content'>
+          <Filters />
+          <CountriesTable />
+        </div>
+      </main>
+    </CountriesProvider>
   )
 }
