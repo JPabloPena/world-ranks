1500db0457c06f5502bedf7f36a06e637dcea778
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-01-04 18:01:57 -0500

    feat(CountriesContext): add Countries context

---
+++ src/contexts/CountriesContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useEffect } from 'react'
+
+export const CountriesContext = createContext()
+
+export function CountriesProvider ({ children }) {
+  const [countries, setCountries] = useState([])
+  const [totalCountries, setTotalCountries] = useState(0)
+
+  const COUNTRIES_API = 'https://restcountries.com/v3.1/all?fields=flags,name,population,area,region,cca3'
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch(COUNTRIES_API)
+        const data = await response.json()
+        setCountries(data)
+        setTotalCountries(data.length)
+      } catch (error) {
+        console.error('Error fetching countries', error)
+      }
+    }
+
+    fetchCountries()
+  }, [])
+
+  return (
+    <CountriesContext.Provider value={{ countries, totalCountries }}>
+      {children}
+    </CountriesContext.Provider>
+  )
+}
