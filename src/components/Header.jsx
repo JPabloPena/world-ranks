722a017132d2769d373ad70483104548050c99f9
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-01-04 18:07:57 -0500

    feat(Header): consume CountriesContext to show the total of countries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react'
 import './Header.css'
+import { CountriesContext } from '../contexts/countriesContext'
 
 export function Header () {
+  const { totalCountries } = useContext(CountriesContext)
+
   return (
     <header className='header'>
-      <p className='header-info'>Found 234 countries</p>
+      <p className='header-info'>Found {totalCountries} countries</p>
       <form>
         <input placeholder='Search by Name, Region, Subregion' />
       </form>
