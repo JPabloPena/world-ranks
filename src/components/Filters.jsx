2317b039f733d8209d369542bf17f4026d35c174
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-01-08 18:30:21 -0500

    feat(Filters): implement sort by and status filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,16 +1,39 @@
+import { useFilters } from '../hooks/useFilters'
 import './Filters.css'
 
 export function Filters () {
   const REGIONS = ['Americas', 'Antartic', 'Africa', 'Asia', 'Europe', 'Oceania']
 
+  const { setFilters } = useFilters()
+
+  const handleChangeSortBy = event => {
+    setFilters(prevState => ({
+      ...prevState,
+      sortBy: event.target.value
+    }))
+  }
+
+  const handleChangeStatus = event => {
+    const statusType = event.target.id
+    const isChecked = event.target.checked
+
+    setFilters(prevState => ({
+      ...prevState,
+      status: {
+        ...prevState.status,
+        [statusType]: isChecked
+      }
+    }))
+  }
+
   return (
     <div className='filters'>
       <section className='filters__sort-by'>
         <h3>Sort by</h3>
-        <select defaultValue='Population'>
-          <option value='Name'>Name</option>
-          <option value='Population'>Population</option>
-          <option value='Area'>Area</option>
+        <select defaultValue='population' onChange={handleChangeSortBy}>
+          <option value='name'>Name</option>
+          <option value='population'>Population</option>
+          <option value='area'>Area</option>
         </select>
       </section>
       <section className='filters__region'>
@@ -26,11 +49,11 @@ export function Filters () {
       <section className='filters__status'>
         <h3>Status</h3>
         <div className='filters__status-option'>
-          <input type='checkbox' value='Member of the United Nations' id='memberUN' />
+          <input type='checkbox' value='Member of the United Nations' id='unMember' onChange={handleChangeStatus} />
           <label className='filters__label'>Member of the United Nations</label>
         </div>
         <div className='filters__status-option'>
-          <input type='checkbox' value='Member of the United Nations' id='independent' />
+          <input type='checkbox' value='Independent' id='independent' onChange={handleChangeStatus} />
           <label className='filters__label'>Independent</label>
         </div>
       </section>
