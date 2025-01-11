f0a05f3b93dcdd35a2aa2e97535f84b7fe8df39d
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-01-11 13:22:03 -0500

    refactor(CountriesTable): replace the grid with table elements

--- src/components/CountriesTable.jsx
+++ src/components/CountriesTable.jsx
@@ -6,26 +6,34 @@ export function CountriesTable () {
   const { countries } = useContext(CountriesContext)
 
   return (
-    <div className='countries-table'>
-      <div className='countries-table__header'>
-        <h3>Flag</h3>
-        <h3>Name</h3>
-        <h3>Population</h3>
-        <h3>Area (km²)</h3>
-        <h3>Region</h3>
-      </div>
-      {countries.map(country => (
-        <div className='countries-table__row' key={country.cca3}>
-          <img
-            src={country.flags.svg}
-            alt={`Flag of ${country.name.common}`}
-          />
-          <p>{country.name.common}</p>
-          <p>{country.population.toLocaleString()}</p>
-          <p>{country.area.toLocaleString()}</p>
-          <p>{country.region}</p>
-        </div>
-      ))}
+    <div className='countries-table-container'>
+      <table className='countries-table'>
+        <thead>
+          <tr className='countries-table__header'>
+            <th>Flag</th>
+            <th>Name</th>
+            <th>Population</th>
+            <th>Area (km²)</th>
+            <th>Region</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map(country => (
+            <tr className='countries-table__row' key={country.cca3}>
+              <td>
+                <img
+                  src={country.flags.svg}
+                  alt={`Flag of ${country.name.common}`}
+                />
+              </td>
+              <td>{country.name.common}</td>
+              <td>{country.population.toLocaleString()}</td>
+              <td>{country.area.toLocaleString()}</td>
+              <td>{country.region}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
