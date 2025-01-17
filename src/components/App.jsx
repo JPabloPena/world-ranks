06d50d52a96a2be38e8e2ebc9f29f3fb38072f2f
Juan Pablo Peña F <pablo27-99@hotmail.com>
2025-01-16 19:15:11 -0500

    feat(App): implement Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { CountriesDashboard } from './CountriesDashboard'
+import { Footer } from './Footer'
 
 function App () {
   return (
     <>
       <h1>World Ranks</h1>
       <CountriesDashboard />
+      <Footer />
     </>
   )
 }
